8a256c0f891b3cc6ca988053026e1f3192bed52b
pstinghua <pstinghua@gmail.com>
2022-10-31 16:23:04 +0800

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by 谢远亮 on 2022/10/31.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
